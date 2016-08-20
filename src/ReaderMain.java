ddfd9d521b66106917244880e1d61a42e2acf75f
alexlied <alexlied@gmail.com>
2016-08-19 17:57:01 -0700

    comment out unneeded method for now

--- src/ReaderMain.java
+++ src/ReaderMain.java
@@ -96,12 +96,12 @@ public class ReaderMain {
 		}
 	}
 
-	public boolean containsChar(String s, char search) {
+/*	public boolean containsChar(String s, char search) {
 		if (s.length() == 0)
 			return false;
 		else
 			return s.charAt(0) == search
 					|| containsChar(s.substring(1), search);
-	}
+	}*/
 
 }
