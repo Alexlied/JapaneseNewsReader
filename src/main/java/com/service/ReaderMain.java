f1da8b588282a3de072b552eaa74b2fcdbc8c4ce
alexlied <alexlied@gmail.com>
2018-02-21 18:16:48 -0800

    Added ability to list multiple kanji found

--- src/main/java/com/service/ReaderMain.java
+++ src/main/java/com/service/ReaderMain.java
@@ -15,33 +15,28 @@ public class ReaderMain {
 
 		File file = new File("newfile.txt");
 
-		// readFromRSS("http://rss.asahi.com/rss/asahi/culture.rdf");
 		// N1 kanji list from http://www.tanos.co.uk/jlpt/skills/kanji/
 
 		String listLocation = "jlptN1Kanji.txt";
-		//http://rss.wor.jp/rss1/yomiuri/latestnews.rdf
-		//https://headlines.yahoo.co.jp/rss/bfj-dom.xml
 		String xmlLocation = "https://rss.wor.jp/rss1/yomiuri/latestnews.rdf";
 
 		String kanjiList = readKanjiList(listLocation);
-		if(kanjiList.isEmpty())
-		{
+		if (kanjiList.isEmpty()) {
 			System.out.println("Kanji list is empty.");
 		}
-		
+
 		String content = readFromRSS(xmlLocation, kanjiList);
-		if(content.isEmpty())
-		{
+		if (content.isEmpty()) {
 			System.out.println("Nothing read from RSS feed.");
 		}
-		createAndProcessRSS(file, content);
+		createTextFile(file, content);
 	}
 
 	/**
 	 * @param file
 	 * @param content
 	 */
-	public static void createAndProcessRSS(File file, String content) {
+	public static void createTextFile(File file, String content) {
 		try (FileOutputStream fop = new FileOutputStream(file)) {
 
 			// if file doesn't exists, then create it
@@ -66,37 +61,40 @@ public class ReaderMain {
 
 	/**
 	 * @param rss
-	 * @param kanji
+	 * @param kanjiList
 	 * @return
 	 * @throws IOException
 	 */
-	public static String readFromRSS(String rss, String kanji)
-			throws IOException {
+	public static String readFromRSS(String rss, String kanjiList) throws IOException {
 		// try {
 		URL rssUrl = new URL(rss);
-		BufferedReader in = new BufferedReader(new InputStreamReader(
-				rssUrl.openStream(), "UTF-8"));
-		String sourceCode = "";
+		BufferedReader in = new BufferedReader(new InputStreamReader(rssUrl.openStream(), "UTF-8"));
+		StringBuilder outputContent = new StringBuilder();
 		String line;
 		boolean isGoodTitle = false;
-		
+
 		while ((line = in.readLine()) != null) {
 			// adds title of news article with selected kanji
 			if (line.contains("<title>")) {
-				
+
 				int firstPos = line.indexOf("<title>");
 				String temp = line.substring(firstPos);
 				temp = temp.replace("<title>", "");
 				int lastPos = temp.indexOf("</title>");
 				temp = temp.substring(0, lastPos);
 
-				for (char ch : kanji.toCharArray()) {
+				for (char ch : kanjiList.toCharArray()) {
 					String singleKanji = Character.toString(ch);
+					StringBuilder listOfFoundKanji = new StringBuilder();
+
 					if (temp.contains(singleKanji)) {
-						sourceCode += temp + " [Found this kanji: "
-								+ singleKanji + "]" + "\n";
+						listOfFoundKanji.append(singleKanji);
+
+						System.out.println("current list:" + listOfFoundKanji.toString());
+
+						outputContent.append(temp + " [Found this kanji: " + singleKanji + "]" + "\n");
 						isGoodTitle = true;
-						break;
+						// break;
 					}
 				}
 			}
@@ -109,12 +107,12 @@ public class ReaderMain {
 				temp = temp.replace("<link>", "");
 				int lastPos = temp.indexOf("</link>");
 				temp = temp.substring(0, lastPos);
-				sourceCode += temp + "\n\n";
+				outputContent.append(temp + "\n\n");
 			}
 		}
 
 		in.close();
-		return sourceCode;
+		return outputContent.toString();
 	}
 
 	/**
@@ -123,8 +121,7 @@ public class ReaderMain {
 	 * @throws FileNotFoundException
 	 * @throws IOException
 	 */
-	public static String readKanjiList(String list)
-			throws FileNotFoundException, IOException {
+	public static String readKanjiList(String list) throws FileNotFoundException, IOException {
 		try (BufferedReader br = new BufferedReader(new FileReader(list))) {
 			StringBuilder sb = new StringBuilder();
 			String line = br.readLine();
