270dabb89ef90db112e8a6d5a08bd7271d3664fc
alexlied <alexlied@gmail.com>
2018-02-24 10:32:59 -0800

    Added simple null check test

--- src/test/java/com/service/ReaderMainTest.java
+++ src/test/java/com/service/ReaderMainTest.java
@@ -1,14 +1,25 @@
 package com.service;
 
-import static org.junit.Assert.*;
+//import static org.hamcrest.MatcherAssert.assertThat;
+
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import org.junit.Test;
 
 public class ReaderMainTest {
 
-//	@Test
-//	public void test() {
-//		fail("Not yet implemented");
-//	}
+	@Test(expected = FileNotFoundException.class)
+	public void whenReadingMissingKanjiListLocationReturnNull() throws FileNotFoundException, IOException {
+		// Arrange
+		String listLocation = "falseLocation.txt";
+		
+		// Act
+		ReaderMain.readKanjiList(listLocation);
+		
+		// Assert
+		
+	}
 
 }
